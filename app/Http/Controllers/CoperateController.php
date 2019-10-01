c0194d346b673551fc473f2ddf2c7195d03ab7fa
kelvin njue <kelvinjuen@gmail.com>
2019-10-01 12:34:19 +0300

    update middleware verify

--- app/Http/Controllers/CoperateController.php
+++ app/Http/Controllers/CoperateController.php
@@ -8,6 +8,16 @@ use Illuminate\Support\Facades\Hash;
 
 class CoperateController extends Controller
 {
+     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware(['auth','verified']);
+    }
+
     /**
      * Display a listing of the resource.
      *
