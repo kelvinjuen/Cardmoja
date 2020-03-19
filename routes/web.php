5574a73cba06de9167f1f4525faeff4378174110
kelvin njue <kelvinjuen@gmail.com>
2020-03-19 14:46:15 +0300

    update  19th march

--- routes/web.php
+++ routes/web.php
@@ -51,4 +51,20 @@ Route::resource('admin', 'AdminController');
 Route::group(['middleware' => ['auth']], function(){
     Route::get('/user', 'GraphController@retrieveUserProfile');
 });
+Route::get('storage/{filename}', function ($filename)
+{
+    $path = storage_path('public/' . $filename);
+
+    if (!File::exists($path)) {
+        abort(404);
+    }
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+
+    return $response;
+});
 
